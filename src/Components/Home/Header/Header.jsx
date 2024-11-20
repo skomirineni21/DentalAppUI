c0c9ecaca8bf1695b13cad406ff70e3eb3055c53
bhasker0909 <b.bhasker.9@gmail.com>
2024-11-19 23:29:29 -0600

    updates

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Container, Col, Row } from "reactstrap";
-import HeaderImg from "../../../Main_images/Image.png";
+import HeaderImg from "../../../Main_images/HP_clinic_img_dark.png";
+// import HeaderImg2 from "../../../Main_images/HP_clinic_img_2.png";
+// import HeaderImg3 from "../../../Main_images/HP_clinic_img_3.png"
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import "./Header.scss";
@@ -9,24 +11,25 @@ import "./Header.scss";
 const Header = () => {
 	return (
 		<div className="header">
-			<Container>
-				<Row className="header-row">
-					<Col md="6" className="headings">
-						<p className="headings__p1">Welcome to Dr. Naveen’s Dental Clinic!</p>
-						<p className="headings__p3">Creating Miles of Smiles</p>
-						<p className="headings__p2">We're dedicated to providing you with quality treatment delivered by our team of highly qualified professionals. Your oral health and well-being are our top priorities, and we strive to offer the best dental care with great care and concern for our patients.</p>			
-						{/*<Link className="headings__btn" to="/">Make Appointment</Link>*/}
-					</Col>
-					<Col md="6" className="header-row__img">
-						<LazyLoadImage 
-						alt="header-row_image"
-						src={HeaderImg}
-						effect="blur"
-						/>
-					</Col>
-				</Row>
-			</Container>
-		</div>
+            <Container>
+                <Row className="header-row">
+                    <Col md="6" className="headings">
+                        <p className="headings__p1">At Dr. Naveen’s Dental Clinic!</p>
+                        <p className="headings__p3">More Than Just Teeth, We Care for Your Smile.</p>
+                        <p className="headings__p2">We're dedicated to providing you with quality treatment delivered by our team of highly qualified professionals. Your oral health and well-being are our top priorities, and we strive to offer the best dental care with great care and concern for our patients.</p>          
+                        <Link className="headings__btn" to="/">Make Appointment</Link>
+                    </Col>
+                    {/* <Col md="12" className="header-row__img">
+                        <LazyLoadImage 
+                        alt="header-row_image"
+                        src={HeaderImg}
+                        effect="blur"
+                        />
+                    </Col> */}
+                </Row>
+            </Container>
+        </div> 
+
 	)
 }
 export default Header;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -6,11 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
-  faYoutube,
+  // faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import "./Navbar.scss";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-
 const Navbar = () => {
   const [show, setShow] = useState(false);
   const [scroll, setScroll] = useState(false);
@@ -22,11 +22,9 @@ const Navbar = () => {
       setScroll(false);
     }
   };
-
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
   });
-
   return (
     <div
       className="navbar-m"
@@ -35,100 +33,107 @@ const Navbar = () => {
       }}
     >
       <Container>
-        <Row>
-          <Col md="2" className="logo" sm="12" xs="12">
-            <Link to="/">
-              <img src={Logo} alt="Dr. Naveen's Dental Clinic" />
-            </Link>
-            <FontAwesomeIcon
-              icon={faBars}
-              className="bar"
-              onClick={() => setShow((prev) => !prev)}
-            />
-          </Col>
-          <Col
-            md="5"
-            className="nav"
-            sm="12"
-            xs="12"
-            style={{
-              top: show ? "79px" : "-100%",
-            }}
-          >
-            <ul className="nav__list">
-              <li className="nav__list_item">
-                <Link to="/about" className="nav__list_item_link">
-                  About
-                </Link>
-              </li>
-              <li className="nav__list_item">
-                <Link to="/doctor" className="nav__list_item_link">
-                  Doctor
-                </Link>
-              </li>
-              <li className="nav__list_item">
-                <Link to="/blog" className="nav__list_item_link">
-                  Blog
-                </Link>
-              </li>
-              <li className="nav__list_item">
-                <Link to="/contact" className="nav__list_item_link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </Col>
-          {/*<Col md="2" className="logo" sm="12" xs="12">
-            <Link to="/">
-              <img src={Logo} alt="LogoIpsum" />
-            </Link>
-            <FontAwesomeIcon
-              icon={faBars}
-              className="bar"
-              onClick={() => setShow((prev) => !prev)}
-            />
-          </Col>*/}
-          <Col
-            md="5"
-            className="socials"
-            sm="12"
-            xs="12"
-            style={{
-              top: show ? "228px" : "-100%",
-            }}
-          >
-            <ul className="list-inline">
-              <li className="list-inline-item">
-                <a
-                  href="https://instagram.com/drnaveens_dental"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faInstagram} />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a
-                  href="https://facebook.com/drnaveensdental"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-              </li>
-              {/* <li className="list-inline-item">
-                <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </li> */}
-              <li className="list-inline-item">
-                <a href="https://youtube.com" target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+        <div className="first-row">
+          <Row className="d-flex align-items-center">
+            <Col md="3" className="logo" sm="3" xs="12">
+              <Link to="/">
+                <img src={Logo} alt="Dr. Naveen's Dental Clinic" />
+              </Link>
+              <FontAwesomeIcon
+                icon={faBars}
+                className="bar"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            </Col>          
+            <Col md="3" className="address d-none d-md-flex" sm="3" xs="12">
+              <a href="https://www.google.com/maps/place/Dr.+Naveen's+Dental+Clinic/@17.5025629,78.5110089,17z/data=!4m14!1m7!3m6!1s0x3bcb9bcb273a37bf:0xd610927f193f92cf!2sDr.+Naveen's+Dental+Clinic!8m2!3d17.5025578!4d78.5135838!16s%2Fg%2F11s8pktdzk!3m5!1s0x3bcb9bcb273a37bf:0xd610927f193f92cf!8m2!3d17.5025578!4d78.5135838!16s%2Fg%2F11s8pktdzk?entry=ttu&g_ep=EgoyMDI0MTAwOS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noreferrer">11-86/1, 1st floor, Opp: ICICI Bank <br />Alwal, Secunderabad, Telanaga</a>
+            </Col>
+            <Col md="3" className="contact d-none d-md-block" sm="3" xs="12">
+              <div>
+                <a href="tel:+918790297437">+91 8790 297 437</a>
+              </div>
+              <div>
+                <a href="tel:+919000796953">+91 9000 796 953</a>
+              </div>
+            </Col>
+            <Col
+              md="2"
+              className="socials"
+              sm="12"
+              xs="12"
+              style={{
+                top: show ? "228px" : "-100%",
+              }}
+            >
+              <ul className="list-inline">
+                <li className="list-inline-item">
+                  <a
+                    href="https://instagram.com/drnaveens_dental"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://facebook.com/drnaveensdental"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faFacebook} />
+                  </a>
+                </li>
+                {/* <li className="list-inline-item">
+                  <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
+                </li> */}
+                {/* <li className="list-inline-item">
+                  <a href="https://youtube.com" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faYoutube} />
+                  </a>
+                </li> */}
+              </ul>
+            </Col>
+          </Row>
+        </div>
+        <div className="second-row">
+          <Row className="d-flex align-items-center">
+            <Col
+              md="5"
+              className="nav"
+              sm="12"
+              xs="12"
+              style={{
+                top: show ? "79px" : "-100%",
+              }}
+            >
+              <ul className="nav__list">
+                <li className="nav__list_item">
+                  <Link to="/about" className="nav__list_item_link">
+                    About
+                  </Link>
+                </li>
+                <li className="nav__list_item">
+                  <Link to="/doctor" className="nav__list_item_link">
+                    Doctor
+                  </Link>
+                </li>
+                <li className="nav__list_item">
+                  <Link to="/blog" className="nav__list_item_link">
+                    Blog
+                  </Link>
+                </li>
+                <li className="nav__list_item">
+                  <Link to="/contact" className="nav__list_item_link">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
